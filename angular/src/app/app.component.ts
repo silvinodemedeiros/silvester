1818082559a73faf8bd5a6c81789fa153d5c35c1
Silvino Medeiros <silvinogustavo@gmail.com>
2025-05-10 13:04:16 -0300

    integrates UI with real time change detection from orion

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { bootstrapClouds } from '@ng-icons/bootstrap-icons';
@@ -8,17 +8,18 @@ import { bootstrapClouds } from '@ng-icons/bootstrap-icons';
 interface Cell {
   row: number;
   col: number;
-  content?: string;
+  item?: string;
 }
 
 interface Widget {
   id: string;
-  label: string;
-  type: string;
-  width: number;
-  height: number;
-  content?: object | null;
-  data?: object | null;
+  item?: null | {
+    id: string;
+    label: string;
+    width: number;
+    height: number;
+  };
+  data?: any | null;
 }
 
 @Component({
@@ -38,9 +39,9 @@ export class AppComponent implements OnInit, OnDestroy {
   apiUrl = 'http://localhost:3000';
 
   widgetItems = [
-    {content: { id: 'w1', label: 'Chart', width: 2, height: 2 }},
-    {content: { id: 'w2', label: 'Table', width: 3, height: 1 }},
-    {content: { id: 'w3', label: 'Text Box', width: 2, height: 1 }},
+    {item: { id: 'w1', label: 'Chart', width: 2, height: 2 }},
+    {item: { id: 'w2', label: 'Table', width: 3, height: 1 }},
+    {item: { id: 'w3', label: 'Text Box', width: 2, height: 1 }},
   ];
 
   rows = 6;
@@ -56,16 +57,41 @@ export class AppComponent implements OnInit, OnDestroy {
     return Object.values(this._widgets);
   }
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(
+    private httpClient: HttpClient,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     // EVENT SOURCE
     
-    const eventSource = new EventSource('http://localhost:3000/events');
-    eventSource.onmessage = (event) => {
-      const notification = JSON.parse(event.data);
-      console.log('📡 Received event:', notification);
+    const widgetSource = new EventSource('http://localhost:3000/events');
+    widgetSource.onmessage = (event) => {
+      const widgetSourceObj = JSON.parse(event.data).data[0];
+      console.log('📡 Received event:', widgetSourceObj);
+
+      // update widgetItems data
+      this.updateWidgetItems(widgetSourceObj);
+      
+      // update grid widgets
+      Object.keys(this._widgets).forEach((key) => {
+        const gridWidget = this._widgets[key];
+        const gridWidgetLabel = gridWidget.item.label;
+
+        if (Object.keys(widgetSourceObj).some((label) => label === gridWidgetLabel)) {
+          this._widgets = {
+            ...this._widgets,
+            [key]: {
+              ...gridWidget,
+              data: {...widgetSourceObj[gridWidgetLabel]}
+            }
+          };
+        }
+      });
+
+      console.log('widget after message', this._widgets);
+      console.log('widgetItems after message', this.widgetItems);
+      this.cd.detectChanges();
     };
 
 
@@ -76,25 +102,10 @@ export class AppComponent implements OnInit, OnDestroy {
     }
 
     const widgetSub = this.httpClient.get<any>(this.apiUrl + '/entities').subscribe((response) => {
-      const widgetData = response[0];
-      console.log(widgetData);
+      const widgetObject = response[0];
+      this.updateWidgetItems(widgetObject);
 
-      this.widgetItems = Object.keys(widgetData).reduce((acc: any[], key, index) => {
-        if(typeof widgetData[key] === 'object') {
-          return [...acc, {
-            content: {
-              id: 'wi' + (index + 1),
-              type: widgetData[key].type,
-              label: key,
-              width: 2,
-              height: 2
-            },
-            data: widgetData
-          }];
-        }
-
-        return acc;
-      }, []);
+      console.log('widget items', this.widgetItems);
     });
 
     this.subscription.add(widgetSub);
@@ -102,6 +113,29 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  updateWidgetItems(widgetsObject: any) {
+    this.widgetItems = Object.keys(widgetsObject).reduce((acc: any[], key, index) => {
+      if(typeof widgetsObject[key] === 'object') {
+        return [...acc, {
+          item: {
+            id: 'wi' + (index + 1),
+            type: widgetsObject[key].type,
+            label: key,
+            width: 2,
+            height: 2
+          },
+          data: {...widgetsObject[key]}
+        }];
+      }
+
+      return acc;
+    }, []);
+  }
+
+  updateWidgetByLabel(label: string) {
+
   }
 
   onDragStart(event: DragEvent, widget: any, moved = false): void {
@@ -124,7 +158,7 @@ export class AppComponent implements OnInit, OnDestroy {
     const data = event.dataTransfer?.getData('application/json');
     if (!data) return;
 
-    const widget: Widget = JSON.parse(data);
+    const widget = JSON.parse(data);
   }
 
   onDrop(event: DragEvent, row: number, col: number): void {
@@ -144,7 +178,8 @@ export class AppComponent implements OnInit, OnDestroy {
       [`${row}${col}`]: {
         row,
         col,
-        content: {...widget.content}
+        item: {...widget.item},
+        data: {...widget.data}
       }
     };
 
@@ -152,6 +187,8 @@ export class AppComponent implements OnInit, OnDestroy {
       const widgetKey = `${widget.row}${widget.col}`;
       delete this._widgets[widgetKey];
     }
+
+    console.log('widgets', this._widgets);
   }
 
   onDragLeave(): void {
